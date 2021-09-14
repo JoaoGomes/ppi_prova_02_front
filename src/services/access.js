90e0d0f5908130f8c239b6e3d8f262c5515c7dd0
João Eduardo Gomes <JoaoGomes@github.com>
2021-09-13 21:59:30 -0300

    Correção de login.

--- src/services/access.js
+++ src/services/access.js
@@ -2,18 +2,23 @@ import axios from 'axios';
 import api from '../services/api';
 
 async function Access_user(data){
-
-    await axios({
-        url: 'http://localhost:3333/login',
-        method: 'POST',
-        data: data
-    })
-    .then(res=>{
-        localStorage.setItem('@App:user', JSON.stringify(res.data.user));
-        localStorage.setItem('@App:accessToken', res.data.accessToken);
-            
-        api.defaults.headers.Authorization = `Bearer ${res.data.accessToken}`;
-    });
+    
+    // Necessário uso de try-catch para caso senha informada seja errada
+    try{
+        await axios({
+            url: 'http://localhost:3333/login',
+            method: 'POST',
+            data: data
+        })
+        .then(res=>{
+            localStorage.setItem('@App:user', JSON.stringify(res.data.user));
+            localStorage.setItem('@App:accessToken', res.data.accessToken);
+                
+            api.defaults.headers.Authorization = `Bearer ${res.data.accessToken}`;
+        });
+    } catch (error){ 
+        console.log(error)
+    }
 
 }
 
